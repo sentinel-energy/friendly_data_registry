3198b198018cc6c93b027b5287e0616a30f3c1fa
Suvayu Ali <fatkasuvayu+linux@gmail.com>
2021-04-19 14:25:36 +0200

    setup.py: also include yaml schemas

--- setup.py
+++ setup.py
@@ -6,11 +6,18 @@ from setuptools import setup, find_packages
 
 setup(
     name="friendly_data_registry",
-    version="20210419",
+    version="20210419.1",
     description="Schema registry for friendly_data",
     url="https://github.com/sentinel-energy/friendly_data_registry",
     packages=find_packages(exclude=["tests"]),
     install_requires=["pyyaml"],
-    package_data={"friendly_data_registry": ["cols/*.json", "idxcols/*.json"]},
+    package_data={
+        "friendly_data_registry": [
+            "cols/*.json",
+            "cols/*.yaml",
+            "idxcols/*.json",
+            "idxcols/*.yaml",
+        ]
+    },
     include_package_data=True,
 )
