2df092afef0821d91484ae734333344f2cb2f3cf
Suvayu Ali <fatkasuvayu+linux@gmail.com>
2021-04-22 14:09:28 +0200

    dev/tag_release.bash: bug fix + patch release option
    - bug fix: include patch releases in glob to match old tags
    - bug fix: match patch releases in sed expression
    - add parameter for patch releases

--- dev/tag_release.bash
+++ dev/tag_release.bash
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-set -o xtrace
+# set -o xtrace
 
-declare VER=$(date +%Y%m%d) TAG=$(date +%Y-%m-%d)
+declare PATCH=${1:+.$1}
+
+declare VER=$(date +%Y%m%d)${PATCH} TAG=$(date +%Y-%m-%d)${PATCH}
 
 # update package metadata
-sed -i -E "s/(version *= *\")(20[0-9]{6})/\1${VER}/" setup.py pyproject.toml
+sed -i -E "s/(version *= *\")(20[0-9]{6}[.0-9]+)/\1${VER}/" setup.py pyproject.toml
 
-declare OLDTAG=$(git tag -l "20??-??-??" | tail -1)
+declare OLDTAG=$(git tag -l "20??-??-??*" | tail -1)
 declare subject="Release ${TAG}" body="$(git log --format="* %s" ${OLDTAG}..)"
 
 git commit -a -m "${subject}" -m "${body}"
