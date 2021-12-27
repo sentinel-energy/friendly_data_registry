4984c29863853f234cb9b4bac99cb3e7b9164381
Suvayu Ali <fatkasuvayu+linux@gmail.com>
2021-12-27 12:29:22 +0100

    Release 2021-12-27
    * __init__.py: rename logger
    * __init__.py: documentation
    * __init__.py: move schema validation to module
    * __init__.py: include title & description in registry API

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="friendly_data_registry",
-    version="20211116",
+    version="20211227",
     description="Schema registry for friendly_data",
     url="https://github.com/sentinel-energy/friendly_data_registry",
     packages=find_packages(exclude=["tests"]),
