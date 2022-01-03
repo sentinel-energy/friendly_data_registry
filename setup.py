1fe6b9ec918fd5b5a63d5fe46148b432855f340d
Suvayu Ali <fatkasuvayu+linux@gmail.com>
2022-01-03 11:47:54 +0100

    Release 2022-01-03
    * Mention Python 3.10 is supported in metadata
    * requirements: handle requirements better

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ requirements = Path("requirements.txt").read_text().strip().split("\n")
 
 setup(
     name="friendly_data_registry",
-    version="20211227",
+    version="20220103",
     description="Schema registry for friendly_data",
     url="https://github.com/sentinel-energy/friendly_data_registry",
     packages=find_packages(exclude=["tests"]),
